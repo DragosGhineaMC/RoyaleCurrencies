106a0cedbe599e19a3986ca3824eefd3d1200645
DragosGhinea <dragos.dev12@gmail.com>
2024-08-21 19:13:02 +0300

    Adding more functions to the main class and refactor existing ones

--- src/main/java/com/dragosghinea/royale/currencies/RoyaleCurrencies.java
+++ src/main/java/com/dragosghinea/royale/currencies/RoyaleCurrencies.java
@@ -1,6 +1,7 @@
 package com.dragosghinea.royale.currencies;
 
 import com.dragosghinea.royale.currencies.exceptions.DuplicateCurrency;
+import org.bukkit.plugin.Plugin;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -25,13 +26,23 @@ public class RoyaleCurrencies {
         return instance;
     }
 
-    public synchronized void registerCurrency(Currency currency) {
+    public synchronized void registerCurrency(Currency currency, boolean overwriteDuplicate) throws DuplicateCurrency {
         String pluginName = currency.getPlugin().getName().toLowerCase();
-        currencies.put(currency.getId(), currency);
-        Currency old = currencies.put(pluginName + ":" + currency.getId(), currency); // in case of duplicate ids but different plugins
-        if (old != null) {
-            throw new DuplicateCurrency("Duplicate currency id: " + currency.getId(), old, currency);
+
+        if (overwriteDuplicate) {
+            currencies.put(pluginName + ":" + currency.getId(), currency);
         }
+        else {
+            Currency old = currencies.putIfAbsent(pluginName + ":" + currency.getId(), currency); // in case of duplicate ids but different plugins
+            if (old != null) {
+                throw new DuplicateCurrency("Duplicate currency id: " + currency.getId(), old, currency);
+            }
+        }
+        currencies.put(currency.getId(), currency);
+    }
+
+    public synchronized void registerCurrency(Currency currency) throws DuplicateCurrency {
+        registerCurrency(currency, false);
     }
 
     public synchronized boolean unregisterCurrency(Currency currency) {
@@ -49,8 +60,20 @@ public class RoyaleCurrencies {
         return currencies.remove(pluginName + ":" + currency.getId()) != null;
     }
 
+    public synchronized void unregisterAllCurrencies() {
+        currencies.clear();
+    }
+
+    public synchronized boolean unregisterAllCurrencies(Plugin plugin) {
+        if (plugin == null) {
+            return false;
+        }
+
+        return currencies.entrySet().removeIf(entry -> entry.getValue().getPlugin().equals(plugin));
+    }
+
     public Currency getCurrency(String id) {
-        return currencies.get(id);
+        return currencies.getOrDefault(id, null);
     }
 
 }
